146baa22d26a37725a37ba306031caee19699c0d
Khoa Nguyen <khoa.nt200804@gmail.com>
2026-03-30 19:04:47 +0700

    Add Profile service API methods
    Create Profile.service.js exporting getProfile, updateProfile, uploadAvatar, and getGameHistory. These functions wrap the http helper to call /users/profile, /users/profile/avatar (multipart/form-data header), and /users/game-history respectively, centralizing profile-related API interactions.

---
+++ frontend/src/pages/Profile/Profile.service.js
@@ -0,0 +1,13 @@
+﻿import http from "../../utils/http.helper.js";
+
+export const getProfile = () => http.get("/users/profile");
+
+export const updateProfile = (data) => http.put("/users/profile", data);
+
+export const uploadAvatar = (formData) =>
+  http.post("/users/profile/avatar", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+
+export const getGameHistory = (params) =>
+  http.get("/users/game-history", { params });

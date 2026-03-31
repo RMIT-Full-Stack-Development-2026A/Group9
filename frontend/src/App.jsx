5123bc7704ffd54d97b8bb8d718bbe92600b1c35
APotato4325 <s4019570@rmit.edu.vn>
2026-04-01 02:37:55 +0700

    updated app.jsx from just placeholder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login/Login.jsx";
+
 function App() {
-  return <h1>Group 9 Project</h1>;
+  return (
+    <Router>
+      <div className="app-container">
+        <header>
+          <h1>Group 9 Project</h1>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

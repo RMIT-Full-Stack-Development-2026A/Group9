332b7d029455d6e85b30cb60d44fb6f95b3280fc
APotato4325 <s4019570@rmit.edu.vn>
2026-03-31 10:24:20 +0700

    updated user model to use mongoose and be usable

--- backend/src/modules/users/user.model.js
+++ backend/src/modules/users/user.model.js
@@ -1,21 +1,29 @@
 //stores account, authentication, and profile information for players and admins
 
-// {
-//   username: { type: String },
-//   email: { type: String, unique: true },
-//   password: { type: String }, // hashed
-//   country: { type: String },
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-//   role: { type: String, enum: ["player", "admin"] },
-//   isActive: { type: Boolean },
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true }, // Hashed 
+  country: { type: String },
+  role: { type: String, enum: ["player", "admin"], default: "player" },
+  isActive: { type: Boolean, default: true },
+  avatar: { type: String },
+  isPremium: { type: Boolean, default: false },
+  walletBalance: { type: Number, default: 0 },
+  
+  //bruteforce protection fields 
+  failedLoginAttempts: { type: Number, default: 0 },
+  lockUntil: { type: Date },
+}, { timestamps: true });
 
-//   avatar: { type: String },
+//hashes password before saving 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-//   isPremium: { type: Boolean },
-//   walletBalance: { type: Number },
-
-//   failedLoginAttempts: { type: Number },
-//   lastFailedLogin: { type: Date },
-
-//   createdAt: { type: Date }
-// }
+export default mongoose.model("User", userSchema);

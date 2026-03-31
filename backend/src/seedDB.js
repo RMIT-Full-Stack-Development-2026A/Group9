014f5001002041690fe0c7d85dd69dff9cb5fced
APotato4325 <s4019570@rmit.edu.vn>
2026-04-01 02:46:48 +0700

    fixed wrong address, added comments, and made code shorter

--- backend/src/seedDB.js
+++ backend/src/seedDB.js
@@ -1,47 +1,44 @@
+
+//THIS IS THE FILE I USE TO SEED THE DB SO DONT TOUCH IT FOR NOW UNTIL WE FINISH TESTING
+
+
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bcrypt from "bcryptjs";
 import User from "./modules/users/user.model.js";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const seedDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       family: 4 
     });
-    console.log("Connected to DB for seeding...");
+    console.log("connected to db");
 
     await User.deleteMany({});
 
-    const hashedPassword = await bcrypt.hash("Password123!", 10);
+    const hashedPassword = await bcrypt.hash("password123", 10);
 
     const seedUsers = [
       {
-        username: "admin_user",
+        username: "admin",
         email: "admin@tictactoang.com",
         password: hashedPassword,
         role: "admin",
       },
       {
-        username: "player_a",
-        email: "player_a@gmail.com",
+        username: "player1",
+        email: "player1@gmail.com",
         password: hashedPassword,
         role: "player",
         isPremium: true,
       },
-      {
-        username: "player_b",
-        email: "player_b@gmail.com",
-        password: hashedPassword,
-        role: "player",
-        isPremium: false,
-      }
+      
     ];
 
     await User.insertMany(seedUsers);
-    console.log("Database Seeded! You can now log in with:");
-    console.log("User: player_a / Pass: Password123!");
+    console.log("db seeded successfully");
     
     process.exit();
   } catch (err) {

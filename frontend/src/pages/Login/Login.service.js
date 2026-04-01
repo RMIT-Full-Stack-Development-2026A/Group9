8162943c19554f17155945341b527cc134e0bb65
Khoa Nguyen <khoa.nt200804@gmail.com>
2026-04-01 23:08:14 +0700

    Use AUTH_ROUTES.LOGIN in login service
    Replace hardcoded "/auth/login" with AUTH_ROUTES.LOGIN and add import from config/api.config.js to centralize API route definitions and avoid magic strings.

--- frontend/src/pages/Login/Login.service.js
+++ frontend/src/pages/Login/Login.service.js
@@ -1,3 +1,4 @@
 import http from "../../utils/http.helper.js";
+import { AUTH_ROUTES } from "../../config/api.config.js";
 
-export const login = (data) => http.post("/auth/login", data);
+export const login = (data) => http.post(AUTH_ROUTES.LOGIN, data);

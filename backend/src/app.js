ea0bab219c711f34ed4a7ccecfbca669a2addfd2
Knr0k0 <yabokugamiyt@gmail.com>
2026-04-01 17:45:54 +0700

    api update

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./modules/auth/auth.routes.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 

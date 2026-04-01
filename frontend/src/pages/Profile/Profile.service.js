360ac5d11f1b078b7eb37234b44c64012dfad9cb
Khoa Nguyen <khoa.nt200804@gmail.com>
2026-04-01 23:09:56 +0700

    Use USER_ROUTES constants in Profile service
    Replace hardcoded user endpoint strings with USER_ROUTES from api.config.js in Profile.service.js. Adds the USER_ROUTES import and updates getProfile, updateProfile, uploadAvatar, and getGameHistory to use the centralized route definitions for easier maintenance and consistency.

--- frontend/src/pages/Profile/Profile.service.js
+++ frontend/src/pages/Profile/Profile.service.js
@@ -1,13 +1,14 @@
 ﻿import http from "../../utils/http.helper.js";
+import { USER_ROUTES } from "../../config/api.config.js";
 
-export const getProfile = () => http.get("/users/profile");
+export const getProfile = () => http.get(USER_ROUTES.PROFILE);
 
-export const updateProfile = (data) => http.put("/users/profile", data);
+export const updateProfile = (data) => http.put(USER_ROUTES.PROFILE, data);
 
 export const uploadAvatar = (formData) =>
-  http.post("/users/profile/avatar", formData, {
+  http.post(USER_ROUTES.AVATAR, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 
 export const getGameHistory = (params) =>
-  http.get("/users/game-history", { params });
+  http.get(USER_ROUTES.GAME_HISTORY, { params });

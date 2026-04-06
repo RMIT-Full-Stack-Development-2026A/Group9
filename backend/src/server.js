28c554fcb4d4dfd31768fd7703bd94e508b8bc69
ThanHai305 <s4045378@rmit.edu.vn>
2026-04-06 14:25:34 +0700

    feat: refactor server.js to use createServer for improved socket initialization and connection handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
+import { createServer } from "node:http";
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import { initSocket } from "./modules/Realtime/socketServer.js";
 
 const PORT = process.env.PORT || 3000;
 connectDB();
 
-app.listen(PORT, () => {
+const server = createServer(app);
+initSocket(server);
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

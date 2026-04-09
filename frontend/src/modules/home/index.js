07104cfa4df62d02c6cc6ee540e24db51b547d1b
ThanHai305 <s4045378@rmit.edu.vn>
2026-04-09 11:59:34 +0700

    feat: update useHome export to named export for consistency

--- frontend/src/modules/home/index.js
+++ frontend/src/modules/home/index.js
@@ -15,6 +15,6 @@
 
 export { default as HomeContent } from "./components/HomeContent/HomeContent.jsx";
 
-export { default as useHome } from "./hooks/useHome.js";
+export { useHome } from "./hooks/useHome.js";
 
 export * as homeService from "./services/home.service.js";

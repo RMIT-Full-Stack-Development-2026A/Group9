0e6009fd2a801b22221cae8e00910f4a4b47ddc1
ThanHai305 <s4045378@rmit.edu.vn>
2026-04-12 17:58:15 +0700

    Fix import paths for admin controller and auth middleware in admin routes

--- backend/src/modules/admin/routes/admin.route.js
+++ backend/src/modules/admin/routes/admin.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import * as adminController from "./admin.controller.js";
-import { authenticate, authorizeRoles } from "../../middlewares/auth.middleware.js";
+import * as adminController from "../controllers/admin.controller.js";
+import { authenticate, authorizeRoles } from "../../../middlewares/auth.middleware.js";
 
 const router = Router();
 

b033b78cb6802527039a9b2ee53c26f13f667ff8
ThanHai305 <s4045378@rmit.edu.vn>
2026-04-15 00:34:31 +0700

    Refactor user interface functions to return DTOs for profile and avatar updates

--- backend/src/modules/user/interface/user.interface.js
+++ backend/src/modules/user/interface/user.interface.js
@@ -1,6 +1,26 @@
 import * as userService from '../services/user.service.js';
+import {
+	createUserResponseDTO,
+	createProfileDTO,
+} from '../dto/user.dto.js';
 
-export const getProfile = (...args) => userService.getProfile(...args);
-export const updateProfile = (...args) => userService.updateProfile(...args);
-export const updateAvatar = (...args) => userService.updateAvatar(...args);
-export const getGameHistory = (...args) => userService.getGameHistory(...args);
+// Expose public functions that return DTOs
+export const getProfile = async (...args) => {
+	const user = await userService.getProfile(...args);
+	return createProfileDTO(user);
+};
+
+export const updateProfile = async (...args) => {
+	const user = await userService.updateProfile(...args);
+	return createProfileDTO(user);
+};
+
+export const updateAvatar = async (...args) => {
+	const user = await userService.updateAvatar(...args);
+	return createUserResponseDTO(user);
+};
+
+export const getGameHistory = async (...args) => {
+	// Game history is likely an array of sessions, not a user
+	return userService.getGameHistory(...args);
+};

a280cd3ffb3aaffed5d5b8ac1e541fc888dbecd6
ThanHai305 <s4045378@rmit.edu.vn>
2026-04-14 22:07:29 +0700

    Refactor exports in profile module for consistency and clarity

--- frontend/src/modules/profile/index.js
+++ frontend/src/modules/profile/index.js
@@ -16,6 +16,6 @@
 
 export { default as ProfileCard } from "./components/ProfileCard/ProfileCard.jsx";
 
-export { default as useProfile } from "./hooks/useProfile.js";
+export { useProfile } from "./hooks/useProfile.js";
 
-export * as profileService from "./services/profile.service.js";
+export * as profileService from "../services/profile.service.js";

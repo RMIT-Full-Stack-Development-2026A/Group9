8e326c6b089255c6585a95dbd1b341e104f4a4f7
ThanHai305 <s4045378@rmit.edu.vn>
2026-04-14 22:06:58 +0700

    Fix merge conflicts in main.jsx and restore import order for consistency

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-//import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './app/App.jsx'
+import './index.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { AuthProvider } from './app/providers/AuthProvider.jsx'
-import { BrowserRouter } from 'react-router-dom'
-=======
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
->>>>>>> origin/Khoa/Profile-Management-&-Game-History
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
       <AuthProvider>
         <App />
       </AuthProvider>
-=======
-      <App />
->>>>>>> origin/Khoa/Profile-Management-&-Game-History
     </BrowserRouter>
   </StrictMode>,
 )

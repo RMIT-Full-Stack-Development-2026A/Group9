4a26557d7e1b996016a4773902a37b38be8091dd
ThanHai305 <s4045378@rmit.edu.vn>
2026-04-25 01:23:30 +0700

    Refactor LocalGameArena to use GameBoard component

--- frontend/src/modules/game/pages/LocalGameArena.jsx
+++ frontend/src/modules/game/pages/LocalGameArena.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import LocalGameBoard from "../components/LocalGameBoard.jsx";
+import GameBoard from "../components/GameBoard/GameBoard.jsx";
 
 export default function LocalGameArena({settings, onAbort, onGameEnd}){
   // settings: { player1, player2, boardSize, boardStyle, firstPlayer }
   // Pass all settings as props for backend session
   return (
     <div>
-      <LocalGameBoard
+      <GameBoard
         {...settings}
         firstPlayer={settings.firstPlayer}
         onAbort={onAbort}

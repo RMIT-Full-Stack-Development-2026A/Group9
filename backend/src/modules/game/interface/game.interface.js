441eb96d062dab9eb9c7395511929515600aaf2a
ThanHai305 <s4045378@rmit.edu.vn>
2026-05-16 20:30:37 +0700

    Add getMovesBySessionId method to GameInterface for session move retrieval

--- backend/src/modules/game/interface/game.interface.js
+++ backend/src/modules/game/interface/game.interface.js
@@ -6,6 +6,8 @@ export const GameInterface = {
 	createSession: gameRepository.createSession,
 	getSessionById: gameRepository.getSessionById,
 	applyMove: gameService.applyMove,
+	toAlgebraicNotation: gameService.toAlgebraicNotation,
 	appendMove: gameRepository.appendMove,
 	abortSession: gameRepository.abortSession,
+	getMovesBySessionId: gameRepository.getMovesBySessionId,
 };

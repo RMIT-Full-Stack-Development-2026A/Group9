000a9d691de755e256cbb41f1e1400c36d604611
ThanHai305 <s4045378@rmit.edu.vn>
2026-05-19 11:02:35 +0700

    Remove unused leaderboard and multiplayer module imports for cleanup

--- backend/src/modules/index.js
+++ backend/src/modules/index.js
@@ -2,8 +2,6 @@ import registerAdminModule from "./admin/index.js";
 import registerAuthModule from "./auth/index.js";
 import registerBillingModule from "./billing/index.js";
 import registerGameModule from "./game/index.js";
-import registerLeaderboardModule from "./leaderboard/index.js";
-import registerMultiplayerModule from "./multiplayer/index.js";
 import registerUserModule from "./user/index.js";
 
 const registrars = [

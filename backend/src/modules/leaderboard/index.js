66aaf16cef9853e5a7f3739c97da41cc9d28953e
ThanHai305 <s4045378@rmit.edu.vn>
2026-05-19 11:00:35 +0700

    Remove Leaderboard module files for cleanup

--- backend/src/modules/leaderboard/index.js
+++
@@ -1,5 +0,0 @@
-import leaderboardRoutes from "./routes/leaderboard.route.js";
-
-export default function registerLeaderboardModule(app) {
-	app.use("/api/leaderboard", leaderboardRoutes);
-}
--- backend/src/modules/leaderboard/interface/leaderboard.interface.js
+++
@@ -1,6 +0,0 @@
-import * as leaderboardService from '../services/leaderboard.service.js';
-
-// Example: Expose a public function for other modules
-export const getLeaderboard = (type, options) => {
-	return leaderboardService.getLeaderboard(type, options);
-};

85d935bad6deec665a09f785fb2ce2bc9d17f5f8
ThanHai305 <s4045378@rmit.edu.vn>
2026-05-19 10:59:59 +0700

    Remove multiplayer module files for cleanup

--- backend/src/modules/multiplayer/interface/multiplayer.interface.js
+++
@@ -1,6 +0,0 @@
-import * as multiplayerService from '../services/multiplayer.service.js';
-
-// Example: Expose a public function for other modules
-export const createRoom = (hostId, options) => {
-	return multiplayerService.createRoom(hostId, options);
-};
